46dd60cf68f73826b2a3b45f3b7a83dd8554575c
Nagy János <nagy@zold.lan>
2024-05-15 10:25:34 +0200

    alma

--- WpfMVVM/View/LillaView.xaml.cs
+++ WpfMVVM/View/LillaView.xaml.cs
@@ -12,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfMVVM.ViewModel;
 
-namespace WpfMVVM
+namespace WpfMVVM.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LillaView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LillaView : UserControl
     {
-        public MainWindow()
+        public LillaView()
         {
             InitializeComponent();
-            this.DataContext = new MainViewModel();
         }
     }
 }
---
+++ WpfMVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMVVM.Command;
+using WpfMVVM.View;
+
+namespace WpfMVVM.ViewModel
+{
+    public class MainViewModel:INotifyPropertyChanged
+    {
+		private object currentView;
+
+		public object CurrentView
+		{
+			get { return currentView; }
+			set { 
+                currentView = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentView"));
+            }
+		}
+
+        private GreenView greenView;
+        private YellowView yellowView;
+        private LillaView lillaView;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged() {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentView"));
+        }
+
+        public RelayCommand YellowViewCommand { get; } 
+        public RelayCommand GreenViewCommand { get; } 
+        public RelayCommand LillaViewCommand { get; } 
+
+        public MainViewModel()
+        {
+
+            yellowView = new YellowView();
+            greenView = new GreenView();
+            lillaView = new LillaView();
+            CurrentView = greenView;
+
+            YellowViewCommand = new RelayCommand(setYellow);
+
+            GreenViewCommand = new RelayCommand(x=>CurrentView=greenView);
+            LillaViewCommand = new RelayCommand(x => CurrentView = lillaView);
+        }
+
+        private void setYellow(object v) { 
+            CurrentView = yellowView;
+        }
+    }
+}
